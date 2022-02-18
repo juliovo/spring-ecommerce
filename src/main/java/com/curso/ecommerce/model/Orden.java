8a70fd583dc21f3c40dc9c178b3d201b1abdccba
Jesús García Hernández <jgarcia@ESOFTDOMAIN.COM>
2022-02-18 16:17:26 -0600

    creacion de clases, mapeo de clases a tablas con JPA y diseño de administrador

--- src/main/java/com/curso/ecommerce/model/Orden.java
+++ src/main/java/com/curso/ecommerce/model/Orden.java
@@ -2,25 +2,50 @@ package com.curso.ecommerce.model;
 
 import java.sql.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ordenes")
 public class Orden {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String numero;
 	private Date fechaCreacion;
 	private Date fechaRecibida;
 	private double total;
 	
+	@ManyToOne
+	private Usuario usuario;
+	
+	@OneToOne(mappedBy = "orden")
+	private DetalleOrden detalle;
+	
 	public Orden() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Orden(Integer id, String numero, Date fechaCreacion, Date fechaRecibida, double total) {
+	
+
+	public Orden(Integer id, String numero, Date fechaCreacion, Date fechaRecibida, double total, Usuario usuario,
+			DetalleOrden detalle) {
 		super();
 		this.id = id;
 		this.numero = numero;
 		this.fechaCreacion = fechaCreacion;
 		this.fechaRecibida = fechaRecibida;
 		this.total = total;
+		this.usuario = usuario;
+		this.detalle = detalle;
 	}
+
+
 
 	public Integer getId() {
 		return id;
@@ -61,6 +86,27 @@ public class Orden {
 	public void setTotal(double total) {
 		this.total = total;
 	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	
+	public DetalleOrden getDetalle() {
+		return detalle;
+	}
+
+
+
+	public void setDetalle(DetalleOrden detalle) {
+		this.detalle = detalle;
+	}
+
+
 
 	@Override
 	public String toString() {
