270b8f70e85ddfbed73d16cbce7d2ca80f91afe9
Jesús García Hernández <jgarcia@ESOFTDOMAIN.COM>
2022-02-23 10:17:33 -0600

    funcionalidad editar

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -1,11 +1,14 @@
 package com.curso.ecommerce.controller;
 
 
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -40,5 +43,14 @@ public class ProductoController {
 		producto.setUsuario(u);
 		productoService.save(producto);
 		return "redirect:/productos";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id) {
+		Producto producto = new Producto();
+		Optional<Producto> optionalProducto = productoService.get(id);
+		producto = optionalProducto.get();
+		LOGGER.info("Modificando producto: {}", producto);
+		return "productos/edit";
 	}
 }
